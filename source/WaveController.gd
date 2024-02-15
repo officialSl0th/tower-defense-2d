35d50ae9ce04f7d841b439f26488d1142d7f9d5d
Sl0th <85708571+officialSl0th@users.noreply.github.com>
2024-02-15 20:15:35 +0100

    add wave label and countdown

--- source/WaveController.gd
+++ source/WaveController.gd
@@ -8,6 +8,8 @@ class_name WaveController
 @onready var _wave_timer: Timer = Timer.new();
 @onready var _entity_group_timer: Timer = Timer.new();
 @onready var _entity_timer: Timer = Timer.new();
+@onready var _wave_label: Label = get_node("/root/Main/CanvasLayer/UIOverlay/LabelInformation/WaveLabel");
+@onready var _countdown_label: Label = get_node("/root/Main/CanvasLayer/UIOverlay/LabelInformation/CountdownLabel");
 
 var _current_wave_index: int = 0;
 var _current_entity_group_index: int = 0;
@@ -32,35 +34,43 @@ func _ready() -> void:
 
 	_wave_timer.start();
 
-func _start_wave() -> void:
-	print("Wave " + str(_current_wave_index) + " started");
+func _process(_delta: float) -> void:
+	var _remaining_time: float = _wave_timer.time_left;
+	if _remaining_time == 0:
+		return;
 
+	var _formatted: String;
+
+	if _remaining_time > 1:
+		_formatted = "%d" % _remaining_time;
+	else:
+		_formatted = "%.1f" % _remaining_time;
+
+	_countdown_label.set_text("Begins in " + _formatted + " seconds");
+
+func _start_wave() -> void:
 	_current_wave = waves.waves[_current_wave_index];
+	_countdown_label.set_text("");
 
 	_entity_group_timer.set_wait_time(_current_wave.time_between_groups);
 	_current_entity_group_index = 0;
 	_start_entity_group();
 
 func _end_wave() -> void:
-	print("Wave " + str(_current_wave_index) + " ended");
-
 	_current_wave_index += 1;
+	_wave_label.set_text("Wave " + str(_current_wave_index + 1));
 
 	if _current_wave_index < len(waves.waves):
 		_wave_timer.set_wait_time(waves.time_between_waves);
 		_wave_timer.start();
 
 func _start_entity_group() -> void:
-	print("Entity group " + str(_current_entity_group_index) + " started");
-
 	_current_entity_group = _current_wave.entity_groups[_current_entity_group_index];
 	_entity_timer.set_wait_time(_current_entity_group.time_between_entities);
 	_entity_timer.start();
 	_spawn_entity();
 
 func _end_entity_group() -> void:
-	print("Entity group " + str(_current_entity_group_index) + " ended");
-
 	_current_entity_group_index += 1;
 	_current_entity_index = 0;
 	_entity_timer.stop();
@@ -83,8 +93,6 @@ func _spawn_entity() -> void:
 	paths[_current_entity_group.path_id].add_child(_entity);
 	_current_entity_index += 1;
 	_entity_list.append(_entity);
-
-	print("Spawned entity " + str(_current_entity_index));
 
 func _entity_removed(entity: PathEntity) -> void:
 	_entity_list.erase(entity);
